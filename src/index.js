0a629a2c58dd8dde7d26243fcdc177121114162d
HmmerHead <gal.almeida42@gmail.com>
2020-11-11 20:52:32 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import body_Parser from 'body-parser';
+import { graphqlHTTP } from 'express-graphql';
+import mongoose from 'mongoose'
+import { buildSchema } from 'graphql';
+import routes from "./routes/index";
+
+const app = express();
+
+app.use(body_Parser.urlencoded({ extended: false}));
+app.use(body_Parser.json());
+app.use('/grapql', graphqlHTTP({
+    schema: buildSchema(`type Query {msg: String}`),
+    rootValue: { msg: () => 'Hello Word' },
+    graphiql: true,
+    pretty: true
+}));
+
+mongoose.connect('mongodb://172.17.0.2:27017/graphql', { useNewUrlParser: true, useUnifiedTopology: true })
+
+routes(app)
+app.listen(3000, () => console.log('FOI !!!!!'))
